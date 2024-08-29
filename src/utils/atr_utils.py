029d4a59d7753156106bc21888362f0be0213605
Markus von Steht <markus.von.steht@gmx.de>
2024-08-29 11:23:37 +0200

    updated utils

--- src/utils/atr_utils.py
+++ src/utils/atr_utils.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import datetime
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 # calculate true range
 def true_range(x):
@@ -30,6 +33,18 @@ def low_range(x):
     else:
         return max((close_p + high) / 2.0 - low, close_p - low, 0)
 
+# calculate high range
+def high_range(x):
+    open = x['Open']
+    high = x['High']
+    low = x['Low']
+    close_p = x['ClosePrev']
+
+    if pd.isna(close_p):
+        return high - (open + low) / 2.0
+    else:
+        return max(high - (close_p + low) / 2.0, high - close_p, 0)
+
 def find_isolated_spikes(series, num_spikes=10, n_distance=5):
     """
     Find isolated maximum spikes in a Pandas series with a safety distance between each event.
@@ -56,7 +71,7 @@ def find_isolated_spikes(series, num_spikes=10, n_distance=5):
         # Find the index of the maximum value in the series
         max_idx = series_copy.idxmax()
         max_value = series_copy[max_idx]
-        #print(f"{num_spikes}: {max_idx} {max_value}")
+        logger.debug(f"{num_spikes}: {max_idx} {max_value}")
 
         # Save the maximum spike (index and value)
         spikes.append((max_idx, max_value))
@@ -66,7 +81,6 @@ def find_isolated_spikes(series, num_spikes=10, n_distance=5):
         end_idx = min(max_seridx, max_idx + distance)
         
         series_copy[start_idx:end_idx] = np.nan
-    print(spikes)
 
     # Convert the result to a DataFrame for better readability
     spikes = list(zip(*spikes))
@@ -74,55 +88,47 @@ def find_isolated_spikes(series, num_spikes=10, n_distance=5):
 
     return series
 
+
 def calc_atr_spikes(yf_df:pd.DataFrame, periods=100):
 
-    # we remove the excess data points before the actual AOI that we want.
+    # We remove the excess data points before the actual AOI that we want.
     stock_df = yf_df[-periods:].copy()
 
-    # create intermediate
+    # Create intermediate
     stock_df['ClosePrev'] = stock_df['Close'].shift(1)
 
-    # calc volas
-    stock_df['TR'] = stock_df.apply(true_range, axis=1)
+    # Calc volas
+    stock_df['HR'] = stock_df.apply(high_range, axis=1)
     stock_df['LR'] = stock_df.apply(low_range, axis=1)
 
-    # clean up
-    stock_df.drop(
-        ['ClosePrev'],
-        axis=1,
-        inplace=True,
-        errors='ignore',
-    )
+    high_markers_series = find_isolated_spikes(stock_df['HR'], num_spikes=10, n_distance=5)
+    low_markers_series = find_isolated_spikes(stock_df['LR'], num_spikes=10, n_distance=5)
 
-    ##---- Full ATR
-
-    outliers = 2
-    #outliers = int(len(stock_df) * 0.05)
-    atr_markers = stock_df['TR'].sort_values(ascending=False)[:outliers]
-
-    ##---- Neg. ATR
-
-    low_atr_markers = stock_df['LR'].sort_values(ascending=False)[:outliers]
-
-    ##---- Isolated Spikes
-
-
-    iso_low_atr_markers = find_isolated_spikes(stock_df['LR'], num_spikes=outliers, n_distance=2)
+    # Merge series to df
+    extrema_df = pd.concat([high_markers_series,low_markers_series], axis=1, keys=['HR', 'LR'])
+    extrema_df.sort_index(ascending=True, axis=0, inplace=True)
 
     ##---- Different means
     scaler = 1.0
 
-    markers_series = iso_low_atr_markers
+    result = []
+    # Take valid extrema
+    for item in ['HR', 'LR']:
+        markers_series = extrema_df[~extrema_df[item].isna()][item]
 
-    arithmetic_mean = markers_series.sum() / len(markers_series) * scaler
-    harmonic_mean = len(markers_series) / (1 / markers_series).sum() * scaler
-    geometric_mean = np.exp(np.log(markers_series).mean()) * scaler
+        arithmetic_mean = markers_series.sum() / len(markers_series) * scaler
+        harmonic_mean = len(markers_series) / (1 / markers_series).sum() * scaler
+        geometric_mean = np.exp(np.log(markers_series).mean()) * scaler
+        extrema_ls = extrema_df[item].nlargest(3).to_list()
+        logger.debug(f""" {item} extrema:
+        {'Arithmetic mean' : <16}: {arithmetic_mean:8.2f}
+        {'Harmonic mean' : <16}: {harmonic_mean:8.2f}
+        {'Geometric mean' : <16}: {geometric_mean:8.2f}
+        {'Extrema' : <16}: {extrema_ls}
+        """)
 
+        result.append(geometric_mean)
+        result += extrema_ls
 
-    print(f"""
-    {'Arithmetic mean' : <16}: {arithmetic_mean:8.3}
-    {'Harmonic mean' : <16}: {harmonic_mean:8.3}
-    {'Geometric mean' : <16}: {geometric_mean:8.3}
-    """)
-
-    return harmonic_mean
+    logger.info(result)
+    return result
